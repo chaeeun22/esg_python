b72253b870776043cfe486793c616e715af065aa
chaeeun22 <139301416+chaeeun22@users.noreply.github.com>
2023-07-19 13:36:27 +0900

    practice

---
+++ day1/main.py
@@ -0,0 +1,24 @@
+random_list = [3, 3.9, "python", True]
+
+print (random_list)
+
+print(random_list[1])
+
+a = "Hello"
+print(a[0])
+print(len(a))
+print(a[:5])
+print(a[1:5])
+print(random_list[2])
+
+print(".".join(a))
+
+b = "    hi     "
+print(b.lower())
+print(b.upper())
+print(b.strip())
+
+c = "Life is too short"
+print(c.replace("short", "long"))
+print(c.split())
+print(c.split("s"))
